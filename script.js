665e52b29768a33d9f2642673b952d5d6168c04a
tiestow <tiestow.disney@gmail.com>
2023-09-26 08:40:42 +0200

    Commit

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+
+let op;
+let temp;
+
 function buttonClick(n) {
     document.querySelector("textarea").value = document.querySelector("textarea").value.concat(n)
 }
 
 function backSpace() {
     document.querySelector("textarea").value = document.querySelector("textarea").value.slice(0, document.querySelector("textarea").value.length - 1)
+}
+
+function clear() {
+    document.querySelector("textarea").value = ""
+}
+
+function multiplication() {
+    op = "mult"
+    temp = document.querySelector("textarea").value
+    document.querySelector("textarea").value = ""
+}
+
+function equals() {
+    switch (op) {
+        case "mult": mult()
+            break;
+        case "add":
+            break;
+        case "sub":
+            break;
+        case "dev":
+            break;
+    }
+}
+
+function mult() {
+    document.querySelector("textarea").value = parseInt(temp * document.querySelector("textarea").value, 10)
 }
