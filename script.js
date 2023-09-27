2a27d12aa86463ed3e326cd5fa6b930d9a3ec0c2
tiestow <tiestow.disney@gmail.com>
2023-09-27 10:18:25 +0200

    Commit

--- script.js
+++ script.js
@@ -14,8 +14,27 @@ function clear() {
     document.querySelector("textarea").value = ""
 }
 
+function addition() {
+    op = "add"
+    temp = document.querySelector("textarea").value
+    document.querySelector("textarea").value = ""
+}
+
+function subtrction() {
+    op = "sub"
+    temp = document.querySelector("textarea").value
+    document.querySelector("textarea").value = ""
+}
+
+
 function multiplication() {
     op = "mult"
+    temp = document.querySelector("textarea").value
+    document.querySelector("textarea").value = ""
+}
+
+function division() {
+    op = "dev"
     temp = document.querySelector("textarea").value
     document.querySelector("textarea").value = ""
 }
@@ -24,15 +43,31 @@ function equals() {
     switch (op) {
         case "mult": mult()
             break;
-        case "add":
+        case "add": add()
             break;
-        case "sub":
+        case "sub": sub()
             break;
-        case "dev":
+        case "dev": div()
             break;
     }
 }
 
 function mult() {
-    document.querySelector("textarea").value = parseInt(temp * document.querySelector("textarea").value, 10)
+    document.querySelector("textarea").value = parseInt(temp) * parseInt(document.querySelector("textarea").value) 
+}
+
+function add() {
+    document.querySelector("textarea").value = parseInt(temp) + parseInt(document.querySelector("textarea").value)
+}
+
+function sub() {
+    document.querySelector("textarea").value = parseInt(temp) - parseInt(document.querySelector("textarea").value)
+}
+
+function div() {
+    if (document.querySelector("textarea").value === "0") {
+        document.querySelector("textarea").value = "Division by Zero not possible"
+    } else {
+        document.querySelector("textarea").value = parseInt(temp) / parseInt(document.querySelector("textarea").value)
+    }
 }
